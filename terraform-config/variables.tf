a3eaf43c02b43db3ac6b3a009b6711f0261570ba
Kev Jimenez <jimenezkevin007@gmail.com>
2024-01-15 10:44:11 +0000

    added variables for cloudflare as dns

--- terraform-config/variables.tf
+++ terraform-config/variables.tf
@@ -3,3 +3,18 @@ variable "instance_name" {
   type        = string
   default     = "testsample"
 }
+
+variable "cf_email" {
+  description = "cloudflare email"
+  type = string
+}
+
+variable "cf_api" {
+  description = "cloudflare api key"
+  type = string
+}
+
+variable "ec2_ip" {
+  description = "ec2 instance ip"
+  type = string
+}
