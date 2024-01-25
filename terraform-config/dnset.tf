7ef0b90a6fde71af87cc59ead815fba0dbf16ccc
Kev Jimenez <jimenezkevin007@gmail.com>
2024-01-25 03:27:22 +0000

    changed resource name for cf

--- terraform-config/dnset.tf
+++ terraform-config/dnset.tf
@@ -4,7 +4,7 @@ provider "cloudflare" {
   api_key = var.cf_api
 }
 
-resource "cloudflare_record" "test" {
+resource "cloudflare_record" "a_record_cf" {
   zone_id = var.zonecf_id
   name    = "17110711.xyz"
   value   = aws_instance.app_server.public_ip
